462ea4def965fff5b1247922f923100f75acab8c
Rickson Lima <lima.rickson20@gmail.com>
2021-09-24 18:43:32 -0400

    add trigger_updated_at and create_table_posts migration file

---
+++ src/database/migrations/03_create_table_posts.js
@@ -0,0 +1,27 @@
+const tableName = 'posts'
+
+exports.up = async function (knex) {
+  await knex.schema.createTable(tableName, (table) => {
+    table.increments('id').primary().unsigned()
+    table.string('title').notNull()
+    table.text('content').notNull()
+    table.string('tags')
+    table.string('url_image')
+
+    table.timestamps(false, true)
+
+    table.integer('author_id').references('id').inTable('users')
+  })
+
+  await knex.raw(`
+    CREATE TRIGGER update_timestamp
+    BEFORE UPDATE
+    ON ${tableName}
+    FOR EACH ROW
+    EXECUTE PROCEDURE update_timestamp();
+  `)
+}
+
+exports.down = function (knex) {
+  return knex.schema.dropTable(tableName)
+}
