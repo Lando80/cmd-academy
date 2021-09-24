3c8a194d0cf3439a2106e12f7067664e478deb7f
Rickson Lima <lima.rickson20@gmail.com>
2021-09-24 10:26:47 -0400

    add morgan as dev-depency and imported  on server.js

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import morgan from 'morgan'
 import cors from 'cors'
 
 import routes from './routes'
@@ -11,6 +12,9 @@ const port = process.env.SERVER_PORT
 
 server.use(cors())
 server.use(express.json())
+server.use(express.urlencoded({ extended: true }))
+server.use(morgan('dev'))
+
 server.use(routes)
 
 server.listen(port, () => {
