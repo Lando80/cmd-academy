c547abe61a8967900567ce439e1c468462d9efce
Rickson Lima <lima.rickson20@gmail.com>
2021-09-25 11:07:00 -0400

    add Post Controller routes

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,26 @@
 import express from 'express'
 import UserController from './app/controllers/UserController'
 import SessionController from './app/controllers/SessionController'
+import PostController from './app/controllers/PostController'
 import AuthMiddleware from './app/middlewares/authentication'
 
 const routes = express.Router()
 
 routes.post('/users', UserController.save)
 routes.post('/auth', SessionController.signin)
-routes.put('/users/:id', AuthMiddleware, UserController.save)
+
+// Authentication middleware
+routes.use(AuthMiddleware)
+
+routes.put('/users/:id', UserController.save)
+
+// Posts routes
+routes.post('/posts', PostController.save)
+routes.put('/posts/:post_id', PostController.update)
+routes.delete('/posts/:post_id', PostController.delete)
+
+routes.get('/posts/:post_id', PostController.show)
+routes.get('/:user_id/posts', PostController.indexAll)
+routes.get('/feed', PostController.index)
 
 export default routes
