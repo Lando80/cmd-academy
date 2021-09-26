34f133109796121e161815e6fe9c7431690263a2
Rickson Lima <lima.rickson20@gmail.com>
2021-09-25 20:12:38 -0400

    feat: User Controller uploads image

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,18 +1,20 @@
 import bcrypt from 'bcrypt'
 import knex from '../../database/connection'
-import { existsOrError, notExistsOrError, equalsOrError } from '../utils/validations.js'
+import {
+  existsOrError,
+  notExistsOrError,
+  equalsOrError,
+} from '../utils/validations.js'
 
-const hashedPassword = password => {
+const hashedPassword = (password) => {
   const salt = bcrypt.genSaltSync(10)
   return bcrypt.hashSync(password, salt)
-} 
+}
 
 class UserController {
- 
   async save(req, res) {
-    
-    const user = {...req.body}
-  
+    const user = { ...req.body }
+
     try {
       existsOrError(user.name, 'Nome não informado')
       existsOrError(user.email, 'E-mail não informado')
@@ -23,51 +25,55 @@ class UserController {
       const userFromDB = await knex
         .select('*')
         .from('users')
-        .where({ email: user.email }).first()
+        .where({ email: user.email })
+        .first()
 
-      if (!user.id){
+      if (!user.id) {
         notExistsOrError(userFromDB, 'Usuário já cadastrado')
       }
-    } catch(msg) {
+    } catch (msg) {
       return res.status(400).send(msg)
     }
 
     user.password = hashedPassword(user.password)
     delete user.confirmPassword
 
-      
-    knex('users').insert(user)
-    .then(_ => res.status(204).send())
-    .catch(err => res.status(500).send(err))
+    knex('users')
+      .insert(user)
+      .then((_) => res.status(204).send())
+      .catch((err) => res.status(500).send(err))
   }
 
   async update(req, res) {
+    const { name, email, profession, bio } = req.body
 
-    const { name, email, url_Avatar, profession, bio} = req.body
+    const { firebaseUrl } = req.file ? req.file : ''
 
     existsOrError(name, 'Nome não informado')
     existsOrError(email, 'E-mail não informado')
 
     const user = await knex
-    .select('*')
-    .from('users')
-    .where({ id: req.userId }).first()
+      .select('*')
+      .from('users')
+      .where({ id: req.userId })
+      .first()
 
-    if(user.id){
+    if (user.id) {
       existsOrError(user, 'Usuário não encontrado.')
-    } 
+    }
 
     user.name = name
     user.email = email
-    user.url_Avatar = url_Avatar
+    user.url_Avatar = firebaseUrl
     user.profession = profession
     user.bio = bio
 
-    knex('users').update(user)
-    .where({ id: req.userId })
-    .then(_ => res.status(204).send())
-    .catch(err => res.status(500).send(err))
+    knex('users')
+      .update(user)
+      .where({ id: req.userId })
+      .then((_) => res.status(200).send(user))
+      .catch((err) => res.status(500).send(err))
   }
 }
-  
+
 export default new UserController()
