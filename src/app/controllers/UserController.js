764bb69cc77fd7ee6dc8a3855c33fb9def97491e
Rickson Lima <lima.rickson20@gmail.com>
2021-10-08 09:58:20 -0400

    refactor: renamed users routes and return functions to json

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,18 +1,20 @@
 import bcrypt from 'bcrypt'
 import knex from '../../database/connection'
-import { existsOrError, notExistsOrError, equalsOrError } from '../utils/validations.js'
+import {
+  existsOrError,
+  notExistsOrError,
+  equalsOrError,
+} from '../utils/validations.js'
 
-const hashedPassword = password => {
+const hashedPassword = (password) => {
   const salt = bcrypt.genSaltSync(10)
   return bcrypt.hashSync(password, salt)
-} 
+}
 
 class UserController {
- 
   async save(req, res) {
-    
-    const user = {...req.body}
-  
+    const user = { ...req.body }
+
     try {
       existsOrError(user.name, 'Nome não informado')
       existsOrError(user.email, 'E-mail não informado')
@@ -23,39 +25,40 @@ class UserController {
       const userFromDB = await knex
         .select('*')
         .from('users')
-        .where({ email: user.email }).first()
+        .where({ email: user.email })
+        .first()
 
-      if (!user.id){
+      if (!user.id) {
         notExistsOrError(userFromDB, 'Usuário já cadastrado')
       }
-    } catch(msg) {
+    } catch (msg) {
       return res.status(400).send(msg)
     }
 
     user.password = hashedPassword(user.password)
     delete user.confirmPassword
 
-      
-    knex('users').insert(user)
-    .then(_ => res.status(204).send())
-    .catch(err => res.status(500).send(err))
+    knex('users')
+      .insert(user)
+      .then((_) => res.status(204).send())
+      .catch((err) => res.status(500).send(err))
   }
 
   async update(req, res) {
-
-    const { name, email, url_Avatar, profession, bio} = req.body
+    const { name, email, url_Avatar, profession, bio } = req.body
 
     existsOrError(name, 'Nome não informado')
     existsOrError(email, 'E-mail não informado')
 
     const user = await knex
-    .select('*')
-    .from('users')
-    .where({ id: req.userId }).first()
+      .select('*')
+      .from('users')
+      .where({ id: req.userId })
+      .first()
 
-    if(user.id){
+    if (user.id) {
       existsOrError(user, 'Usuário não encontrado.')
-    } 
+    }
 
     user.name = name
     user.email = email
@@ -63,54 +66,48 @@ class UserController {
     user.profession = profession
     user.bio = bio
 
-    knex('users').update(user)
-    .where({ id: req.userId })
-    .then(_ => res.status(204).send())
-    .catch(err => res.status(500).send(err))
+    knex('users')
+      .update(user)
+      .where({ id: req.userId })
+      .then((_) => res.status(204).send())
+      .catch((err) => res.status(500).send(err))
   }
 
   async get(req, res) {
-
     try {
-
-      const userFromDB = await knex
-        .select('id', 'name', 'email', 'url_Avatar', 'isAdmin', 'profession', 'bio')
+      const usersFromDB = await knex
+        .select('id', 'name', 'email', 'url_Avatar', 'profession', 'bio')
         .from('users')
 
-        return res.status(200).send(userFromDB)
-    } catch(msg) {
-        return res.status(400).send(msg)
+      return res.status(200).json(usersFromDB)
+    } catch (msg) {
+      return res.status(400).json({ error: msg })
     }
   }
 
   async getById(req, res) {
-
     try {
       const userFromDB = await knex
-        .select('id', 'name', 'email', 'url_Avatar', 'isAdmin', 'profession', 'bio')
+        .select('id', 'name', 'email', 'url_Avatar', 'profession', 'bio')
         .from('users')
-        .where({id: req.params.id})
+        .where({ id: req.params.id })
         .first()
 
-        return res.status(200).send(userFromDB)
-
-    } catch(msg) {
-      return res.status(400).send(msg)
+      return res.status(200).json(userFromDB)
+    } catch (msg) {
+      return res.status(400).json({ error: msg })
     }
   }
 
   async countUsers(req, res) {
     try {
-      const userFromDB = await knex
-        .count('id')
-        .from('users')
-        .first()
+      const usersFromDB = await knex.count('id').from('users').first()
 
-      return res.status(200).send(userFromDB)
-    } catch(msg) {
-      return res.status(400).send(msg)
+      return res.status(200).json(usersFromDB)
+    } catch (msg) {
+      return res.status(400).json({ error: msg })
     }
   }
 }
-  
+
 export default new UserController()
--- src/routes.js
+++ src/routes.js
@@ -5,17 +5,15 @@ import AuthMiddleware from './app/middlewares/authentication'
 
 const routes = express.Router()
 
-routes.post('/signup', UserController.save)
-routes.post('/signin', SessionController.signin)
+routes.post('/users', UserController.save)
+routes.post('/auth', SessionController.signin)
 
-routes.get('/count', UserController.countUsers)
+routes.get('/users/count', UserController.countUsers)
 
 routes.use(AuthMiddleware)
 
 routes.put('/users', UserController.update)
 routes.get('/users', UserController.get)
 routes.get('/users/:id', UserController.getById)
-
-
 
 export default routes
